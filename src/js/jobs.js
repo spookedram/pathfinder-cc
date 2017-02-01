46cf2734c16735706908194099998b1cdde58c5a
Zach Allen <zallen@aetn.org>
2017-02-01 10:26:28 -0600

    downloaded src from c9.io and added to repo

---
+++ src/js/jobs.js
@@ -0,0 +1,170 @@
+var jobs = [
+    {
+        "name":"Barbarian",
+        "desc":"Big scary guy",
+        "wpns":"Big ass axe",
+        "armor":"None!",
+        "suit":"Close-range combat",
+        "alignments":"CG, CN, CE"
+    },
+    {
+        "name":"Bard",
+        "desc":"Singin' fool",
+        "wpns":"Big ass lute",
+        "armor":"None!",
+        "suit":"Long to Mid-range buffing",
+        "alignments":"CG, CN, CE"
+    },
+    {
+        "name":"Cleric",
+        "desc":"A craaaazy guy",
+        "wpns":"Big ass axe",
+        "armor":"None!",
+        "suit":"Close-range combat",
+        "alignments":"CG, CN, CE"
+    },
+    {
+        "name":"Druid",
+        "desc":"A craaaazy guy",
+        "wpns":"Big ass axe",
+        "armor":"None!",
+        "suit":"Close-range combat",
+        "alignments":"CG, CN, CE"
+    },
+    {
+        "name":"Fighter",
+        "desc":"A craaaazy guy",
+        "wpns":"Big ass axe",
+        "armor":"None!",
+        "suit":"Close-range combat",
+        "alignments":"CG, CN, CE"
+    },
+    {
+        "name":"Monk",
+        "desc":"A craaaazy guy",
+        "wpns":"Big ass axe",
+        "armor":"None!",
+        "suit":"Close-range combat",
+        "alignments":"CG, CN, CE"
+    },
+    {
+        "name":"Paladin",
+        "desc":"A craaaazy guy",
+        "wpns":"Big ass axe",
+        "armor":"None!",
+        "suit":"Close-range combat",
+        "alignments":"CG, CN, CE"
+    },
+    {
+        "name":"Ranger",
+        "desc":"A craaaazy guy",
+        "wpns":"Big ass axe",
+        "armor":"None!",
+        "suit":"Close-range combat",
+        "alignments":"CG, CN, CE"
+    },
+    {
+        "name":"Rogue",
+        "desc":"A craaaazy guy",
+        "wpns":"Big ass axe",
+        "armor":"None!",
+        "suit":"Close-range combat",
+        "alignments":"CG, CN, CE"
+    },
+    {
+        "name":"Sorcerer",
+        "desc":"A craaaazy guy",
+        "wpns":"Big ass axe",
+        "armor":"None!",
+        "suit":"Close-range combat",
+        "alignments":"CG, CN, CE"
+    },
+    {
+        "name":"Wizard",
+        "desc":"A craaaazy guy",
+        "wpns":"Big ass axe",
+        "armor":"None!",
+        "suit":"Close-range combat",
+        "alignments":"CG, CN, CE"
+    },
+    {
+        "name":"Arcane Archer",
+        "desc":"A craaaazy guy",
+        "wpns":"Big ass axe",
+        "armor":"None!",
+        "suit":"Close-range combat",
+        "alignments":"CG, CN, CE"
+    },
+    {
+        "name":"Arcane Trickster",
+        "desc":"A craaaazy guy",
+        "wpns":"Big ass axe",
+        "armor":"None!",
+        "suit":"Close-range combat",
+        "alignments":"CG, CN, CE"
+    },
+    {
+        "name":"Assassin",
+        "desc":"A craaaazy guy",
+        "wpns":"Big ass axe",
+        "armor":"None!",
+        "suit":"Close-range combat",
+        "alignments":"CG, CN, CE"
+    },
+    {
+        "name":"Dragon Disciple",
+        "desc":"A craaaazy guy",
+        "wpns":"Big ass axe",
+        "armor":"None!",
+        "suit":"Close-range combat",
+        "alignments":"CG, CN, CE"
+    },
+    {
+        "name":"Duelist",
+        "desc":"A craaaazy guy",
+        "wpns":"Big ass axe",
+        "armor":"None!",
+        "suit":"Close-range combat",
+        "alignments":"CG, CN, CE"
+    },
+    {
+        "name":"Eldritch Knight",
+        "desc":"A craaaazy guy",
+        "wpns":"Big ass axe",
+        "armor":"None!",
+        "suit":"Close-range combat",
+        "alignments":"CG, CN, CE"
+    },
+    {
+        "name":"Loremaster",
+        "desc":"A craaaazy guy",
+        "wpns":"Big ass axe",
+        "armor":"None!",
+        "suit":"Close-range combat",
+        "alignments":"CG, CN, CE"
+    },
+    {
+        "name":"Mystic Theurge",
+        "desc":"A craaaazy guy",
+        "wpns":"Big ass axe",
+        "armor":"None!",
+        "suit":"Close-range combat",
+        "alignments":"CG, CN, CE"
+    },
+    {
+        "name":"Pathfinder Chronicler",
+        "desc":"A craaaazy guy",
+        "wpns":"Big ass axe",
+        "armor":"None!",
+        "suit":"Close-range combat",
+        "alignments":"CG, CN, CE"
+    },
+    {
+        "name":"Shadowdancer",
+        "desc":"A craaaazy guy",
+        "wpns":"Big ass axe",
+        "armor":"None!",
+        "suit":"Close-range combat",
+        "alignments":"CG, CN, CE"
+    }
+];
